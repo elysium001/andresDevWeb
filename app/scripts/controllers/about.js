9132a28c3a33e92f173170e48b0d550283de194b
Omar Serrano <a.omarserrano@gmail.com>
2015-10-05 23:32:39 -0500

    adding angular web folder

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name andresWebDevApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the andresWebDevApp
+ */
+angular.module('andresWebDevApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name andresWebDevApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the andresWebDevApp
+ */
+angular.module('andresWebDevApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
