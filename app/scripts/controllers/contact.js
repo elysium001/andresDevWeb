1400df99ec6248e9e9a0d6a6e43b700abe4c544b
Omar Serrano <a.omarserrano@gmail.com>
2015-10-06 01:22:27 -0500

    Adding new contact controller and view with image

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name andresWebDevApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the andresWebDevApp
+ */
+angular.module('andresWebDevApp')
+  .controller('ContactCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
