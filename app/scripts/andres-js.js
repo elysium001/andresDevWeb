42fe7fed1255c3c00d24ee244c06bdaafdbb8dfe
Omar Serrano <a.omarserrano@gmail.com>
2015-10-11 03:09:14 -0500

    Uploading new hp, about, and contact style design'

---
+++ app/scripts/andres-js.js
@@ -0,0 +1,33 @@
+$(function() {
+
+	// variables 
+	var browserWidth = $(window).width();
+	var isMobile = browserWidth < 600 ? true : false;
+	var isTablet = browserWidth < 1025 && browserWidth > 767 ? true : false;
+	var isNormal = browserWidth > 1024 ? true : false;
+
+	// third slide city function!
+	var city = "";
+
+	$.get("http://ipinfo.io", function(response) {
+		city = response.city + "'s";
+		$("#city-name").text(city);
+	}, "jsonp");
+
+
+	// smooth anchor scroll
+	$('a[href*="#webtopic"]').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top
+	    }, 500);
+	    return false;
+	});
+
+	// image preloading
+	var curImg = new Image();
+
+	curImg.src = "/images/meandg.png";
+
+
+
+});
